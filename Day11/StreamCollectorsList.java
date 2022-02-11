8c93a98d7c5759009e1d25b818cb6cfd7fa97526
S_BHARADWAJ_CAPG2022 <83962241+codester4u@users.noreply.github.com>
2022-02-11 15:22:19 +0530

    Add files via upload

---
+++ Day11/StreamCollectorsList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamCollectorsList {
+
+	public static void main(String[] args) {
+	
+	List<Integer>l=new ArrayList<Integer>();
+	for(int i=1;i< 10;i++) {
+		l.add(i);
+	}
+	Stream<Integer>stream=l.stream();
+	List<Integer>evennum=stream.filter(i->i%2==0).collect(Collectors.toList());
+	System.out.println(evennum);
+	}
+
+}
